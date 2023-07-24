6c6e762b936c36cfae223d3ff27c6252d59bf274
adalage <alexis.dalage@gmail.com>
2023-07-24 23:54:13 +0200

    fix CORS

--- chess-api/src/main/java/com/chess/controller/BoardController.java
+++ chess-api/src/main/java/com/chess/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.chess.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ public class BoardController {
     @Autowired
     private RulesService rulesService;
 
+    @CrossOrigin(origins = "http://localhost:42000")
     @GetMapping
     public List<Piece> getChessBoard() {
         return board.getPieces();
