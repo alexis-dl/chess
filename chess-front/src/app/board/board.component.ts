18dec1d2c8936633b29796a9aae5160adc487493
adalage <alexis.dalage@gmail.com>
2023-07-24 23:55:55 +0200

    call getChessBoard api to display chess pieces

--- chess-front/src/app/board/board.component.ts
+++ chess-front/src/app/board/board.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-board',
@@ -6,7 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent {
+
   getSquareColor(row: number, col: number) {
     return (row + col) % 2 == 0 ? 'square light' : 'square dark';
   }
+
+
+  apiData: any;
+
+  private boardUrl = "http://127.0.0.1:8080";  // URL to board api
+  // ça serait bien d'avoir une api qui envoie la pièce d'une case donnée :)
+
+  constructor(
+    private http: HttpClient) {
+    this.getData();
+
+  }
+
+  getPiece(row: number, col: number): string {
+    const piece = this.apiData.find((p: { square: { x: number; y: number; }; }) => p.square.x === col && p.square.y === row);
+    if (piece) {
+      return `${piece.color.toLowerCase()}-${piece.type.toLowerCase()}`;
+    } else {
+      return "";
+    }
+
+  }
+
+  getData() {
+    const url = `${this.boardUrl}/chessboard`;
+    this.http.get<any>(url).subscribe(data => {
+      this.apiData = data;
+    });
+  }
+
+
+
+
+
+  // /** GET pieces from the server */
+  // getPieceFromApi(row: number, col: number): {
+  //   const url = `${this.boardUrl}/piece/${row}/${col}`;
+  //   this.http.get<any>(this.boardUrl)
+  // }
+
 }
