9596b19fda6e654f6b3333741de5c8c609f6f0cb
adalage <alexis.dalage@gmail.com>
2023-08-30 00:19:09 +0200

    improve dispostion of board and player panels

--- chess-front/src/app/app.module.ts
+++ chess-front/src/app/app.module.ts
@@ -7,11 +7,18 @@ import { ChessBoardComponent } from './chess-board/chess-board.component';
 import { ChessSquareComponent } from './chess-square/chess-square.component';
 import { ChessPieceComponent } from './chess-piece/chess-piece.component';
 import { PlayerPanelComponent } from './player-panel/player-panel.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
-  declarations: [AppComponent, ChessBoardComponent, ChessSquareComponent, ChessPieceComponent, PlayerPanelComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    ChessBoardComponent,
+    ChessSquareComponent,
+    ChessPieceComponent,
+    PlayerPanelComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, DragDropModule],
 })
 export class AppModule {}
