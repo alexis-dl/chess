6e54402dab9c62e5868afffbddda9728164cafc3
adalage <alexis.dalage@gmail.com>
2023-12-23 22:53:18 +0100

    merge chess square component wth chess board

--- chess-front/src/app/app.module.ts
+++ chess-front/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChessBoardComponent } from './chess-board/chess-board.component';
-import { ChessSquareComponent } from './chess-square/chess-square.component';
 import { ChessPieceComponent } from './chess-piece/chess-piece.component';
 import { PlayerPanelComponent } from './player-panel/player-panel.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
@@ -13,7 +12,6 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
   declarations: [
     AppComponent,
     ChessBoardComponent,
-    ChessSquareComponent,
     ChessPieceComponent,
     PlayerPanelComponent,
   ],
